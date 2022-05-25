476c8d7c190268253541c83b2b51055eb1b8fb89
tripham97 <minhtri2805@gmail.com>
2022-05-25 01:18:01 -0500

    first commit

---
+++ model/Report.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const documentSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    officialName: {
+        type: String,
+        enum: {
+            values: [
+                "buying_agreement",
+                "selling_agreement",
+                "TC_buying",
+                "TC_selling",
+                "TC_user",
+                "privacy_policy",
+            ],
+            message: "{VALUE} is not a valid official name",
+        },
+        required: true,
+    },
+    url: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Document", documentSchema);
